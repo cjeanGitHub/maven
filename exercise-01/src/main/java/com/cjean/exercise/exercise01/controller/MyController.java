c43a3ce9c36f5535f98b7f3f1ca84867901519cf
cjeanGith <1417261875@qq.com>
2020-03-28 18:40:37 +0800

    new commit

--- exercise-01/src/main/java/com/cjean/exercise/exercise01/controller/MyController.java
+++ exercise-01/src/main/java/com/cjean/exercise/exercise01/controller/MyController.java
@@ -11,16 +11,21 @@ public class MyController {
  
     @RequestMapping("/index")
     public String index(){
-        return "index";
+        return "test";
     }
 
     @RequestMapping("/login")
     public String login(){
-        return "login";
+        return "index-01";
     }
 
     @RequestMapping("/login2")
     public String login2(){
-        return "login2";
+        return "index-02";
+    }
+
+    @RequestMapping("/login3")
+    public String login3(){
+        return "index-thymleaf";
     }
 }
