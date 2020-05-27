9d985202117d2a05a75a0a53361a130267364f96
chudongliang <chudongliang@gmail.com>
2020-05-27 17:35:04 +0800

    分析桶排序02

--- exercise-01/src/main/java/com/cjean/exercise/exercise01/algorithm/heapsoft/HeapSort.java
+++ exercise-01/src/main/java/com/cjean/exercise/exercise01/algorithm/heapsoft/HeapSort.java
@@ -11,12 +11,18 @@ public class HeapSort {
         int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1};// 9
         int[] arr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int[] arr3 = {1, 2, 3, 7, 5, 6, 4, 8, 9};
+        int[] arr4 = {3, 2, 4, 1, 5, 11, 13, 10, 19};
+        int[] arr5 = {3, 2, 4, 1, 5};
+        int[] arr6 = {3, 2, 4, 1, 5, 7 , 10};
 //        sort(arr);
 //        sort(arr3);
 //        System.out.println(Arrays.toString(arr));
 //        System.out.println(Arrays.toString(arr3));
 //        sort(arr2);
-        sort(arr3);
+//        sort(arr3);
+//        sort(arr4);
+//        sort(arr5);
+        sort(arr6);
     }
 
     public static void sort(int[] arr) {
@@ -27,7 +33,7 @@ public class HeapSort {
             //从第一个非叶子结点从下至上，从右至左调整结构
             adjustHeap(arr, i, arr.length);
 
-            System.out.println("first:"+i);
+            System.out.println("first:" + i);
             System.out.println(Arrays.toString(arr));
         }
         System.out.println("==================");
@@ -38,13 +44,14 @@ public class HeapSort {
             System.out.println("-------------");
             adjustHeap(arr, 0, j);//重新对堆进行调整
             System.out.println(Arrays.toString(arr));
-            System.out.println("second:"+j);
+            System.out.println("second:" + j);
         }
 
     }
 
+
     /**
-     * 调整大顶堆（仅是调整过程，建立在大顶堆已构建的基础上）
+     * 调整小顶堆（仅是调整过程，建立在小顶堆已构建的基础上）
      *
      * @param arr
      * @param i
@@ -54,10 +61,34 @@ public class HeapSort {
         int temp = arr[i];//先取出当前元素i
         //k * 2 + 1 获取k索引下的左节点，由于堆是完全二叉树的原因可以认为，k没有左节点就是>length
         for (int k = i * 2 + 1; k < length; k = k * 2 + 1) {//从i结点的左子结点开始，也就是2i+1处开始
+            if (k + 1 < length && arr[k] > arr[k + 1]) {//如果左子结点小于右子结点，k指向右子结点
+                k++;
+            }
+            if (arr[k] < temp) {//如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
+                arr[i] = arr[k];
+                i = k;
+            } else {
+                break;
+            }
+        }
+        arr[i] = temp;//将temp值放到最终的位置
+    }
+
+    /**
+     * 调整大顶堆（仅是调整过程，建立在大顶堆已构建的基础上）
+     *
+     * @param arr
+     * @param i
+     * @param length
+     */
+    public static void adjustHeap2(int[] arr, int i, int length) {
+        int temp = arr[i];//先取出当前元素i
+        //k * 2 + 1 获取k索引下的左节点，由于堆是完全二叉树的原因可以认为，k没有左节点就是>length
+        for (int k = i * 2 + 1; k < length; k = k * 2 + 1) {//从i结点的左子结点开始，也就是2i+1处开始
             if (k + 1 < length && arr[k] < arr[k + 1]) {//如果左子结点小于右子结点，k指向右子结点
                 k++;
             }
-            if (arr[k] > temp) {//如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
+            if (arr[k] < temp) {//如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
                 arr[i] = arr[k];
                 i = k;
             } else {
