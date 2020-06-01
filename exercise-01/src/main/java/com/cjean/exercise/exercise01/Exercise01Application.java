0bd256e1a9aef81e81267e7c16b12b5d5a606be8
chudongliang <chudongliang@gmail.com>
2020-06-01 11:00:30 +0800

    桶排序算法

--- exercise-01/src/main/java/com/cjean/exercise/exercise01/Exercise01Application.java
+++ exercise-01/src/main/java/com/cjean/exercise/exercise01/Exercise01Application.java
@@ -2,12 +2,30 @@ package com.cjean.exercise.exercise01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
+@Controller
 public class Exercise01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Exercise01Application.class, args);
+    }
+
+
+    /**
+     * 小程序生成订单
+     */
+    @ResponseBody
+    @RequestMapping("/netapp")
+    public String generateOrder() {
+        return "hello net app....";
+
     }
 
 }
